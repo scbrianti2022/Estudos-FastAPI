cac5ae375e83890951cc417b80dbbb3aa9f7b961
Sidnei Brianti <scbrianti@outlook.com>
2022-02-24 20:50:51 -0300

    main

--- Basico/main.py
+++ Basico/main.py
@@ -1,2 +1,24 @@
 from typing import Optional
 from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(title='FastAPI Básica',
+              version='0.0.1')
+
+
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, p: bool, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q, "p": p}
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host='0.0.0.0',port=5000, reload=True)
