19bae3732fadbb24e18d0cfba07c78a6e7795466
Sidnei Brianti <scbrianti@outlook.com>
2022-02-25 20:03:07 -0300

    main

--- Basico/main.py
+++ Basico/main.py
@@ -19,6 +19,4 @@ def read_item(item_id: int, p: bool, q: Optional[str] = None):
     return {"item_id": item_id, "q": q, "p": p}
 
 if __name__ == '__main__':
-    import uvicorn
-
-    uvicorn.run("main:app", host='0.0.0.0',port=5000, reload=True)
+    uvicorn.run("main:app", port=5000, reload=True)
